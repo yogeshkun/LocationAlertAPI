a8c7cb3945dcda5d32e3c0ed44b9ba1911d10e7d
Yogesh <yogeshkunkawlekar12@gmail.com>
2023-03-10 01:24:30 +0530

    chnaged url

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
+const url = 'mongodb+srv://yogeshkunkawlekar12:Yogeshkun509@cluster0.wshvsh5.mongodb.net/mylocationapi?retryWrites=true&w=majority'
 mongoose
-  .connect(process.env.DATABASE_URL, {
+  .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
